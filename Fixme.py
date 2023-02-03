f1140a2b808577afae73cb449a24a5abc3a65e03
afr13dman <akfriedman13@gmail.com>
2023-02-03 09:55:47 -0800

    added whitespace

--- Fixme.py
+++ Fixme.py
@@ -6,7 +6,7 @@ def evens(n):
     Returns a list of even numbers from 0 to n inclusive.
     '''
     evens = []
-    for i in range(0, n+1):
+    for i in range(0, n + 1):
         if i % 2 == 0:
             evens.append(i)
     return evens
